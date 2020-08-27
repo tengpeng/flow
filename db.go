449259b3117be28e899db456e2222072489a41a0
Teng Peng <josephtengpeng@gmail.com>
2020-08-27 10:39:56 +0800

    clean up

--- db.go
+++ db.go
@@ -22,16 +22,12 @@ func openDB() {
 	var err error
 	db, err = gorm.Open("sqlite3", "flow.db")
 	if err != nil {
-		fmt.Println(err.Error())
-		panic("failed to connect database")
+		log.Error(err)
 	}
 	err = os.Chmod("flow.db", 0644)
 	if err != nil {
-		log.Println(err)
+		log.Error(err)
 	}
-	//defer db.Close()
-
-	db = db.Set("gorm:auto_preload", true)
 }
 
 func initialMigration() {
@@ -40,25 +36,11 @@ func initialMigration() {
 		fmt.Println(err.Error())
 		panic("failed to connect database")
 	}
-	defer db.Close()
 
 	db.AutoMigrate(&Flow{}, &Target{}, &Task{}, &dep{}, FlowRun{}, &TaskRun{})
+	db = db.Set("gorm:auto_preload", true)
 }
 
-// func setUpTestDB() {
-// 	t := &Target{Name: "test", User: "root", Password: "z", IP: "0.0.0.0"}
-
-// 	t1 := Task{FlowName: "wf1", Name: "nb1", Path: "data/nb1.ipynb", Next: "nb3"}
-// 	t2 := Task{FlowName: "wf1", Name: "nb2", Path: "data/nb2.ipynb", Next: "nb3"}
-// 	t3 := Task{FlowName: "wf1", Name: "nb3", Path: "data/nb3.ipynb"}
-
-// 	f := &Flow{FlowName: "wf1", Schedule: "* * * * *", Tasks: []Task{t1, t2, t3}}
-
-// 	db.Create(f)
-// 	db.Create(t)
-// }
-
-//different entry point
 func pollData() {
 	for {
 		var rs []Target
@@ -81,7 +63,6 @@ func pollData() {
 				continue
 			}
 
-			//TODO: handle empty return
 			var frs []FlowRun
 			err = json.Unmarshal(b, &frs)
 			if err != nil {
