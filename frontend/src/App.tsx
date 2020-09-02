9cdc9b13f2216976351078a46a462e8b18194961
Teng Peng <josephtengpeng@gmail.com>
2020-09-02 15:31:15 +0800

    add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { RecoilRoot } from 'recoil';
 import './App.css';
 import { Grid } from './components/grid';
+import { NavBar } from './components/navbar';
 
 function App() {
     return (
         <RecoilRoot>
+            <NavBar />
             <Grid />
         </RecoilRoot>
     );
